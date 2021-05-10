88073a155a7091a143f6c31dd532fd527fcf5056
Kyle Kochanek <the_designer@kylekochanek.com>
2021-05-10 23:42:41 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Post } from "../components/Post";
+import { posts } from "../getAllPosts";
+
 
 export default function Home() {
   return (
@@ -18,7 +21,11 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+        <div>
+          {posts.map((post) => (
+          <Post key={post.link} post={post} />
+        ))}
+        </div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
@@ -48,6 +55,14 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <div>
+    <p style={{ textAlign: 'right' }}>
+      <a rel="alternate" type="application/rss+xml" href="/static/rss-feed.xml">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.9 16.9A3 3 0 007 21 3 3 0 003 17zm2.8 2.8a1 1 0 11-1.4-1.4 1 1 0 011.4 1.4zM5 12a1 1 0 000 2 5 5 0 015 5 1 1 0 102 0 7 7 0 00-7-7zm0-4a1 1 0 000 2 9 9 0 019 9 1 1 0 102 0A11 11 0 005 8zm10.6.4A15.1 15.1 0 005 4a1 1 0 000 2 13 13 0 0113 13 1 1 0 102 0c0-4-1.6-7.8-4.4-10.6z" fill="currentColor"/></svg>
+      </a>
+      {' '}
+    </p>
+  </div>
       </main>
 
       <footer className={styles.footer}>
